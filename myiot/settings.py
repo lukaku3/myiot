0e8e46db76422204941ff83b83a0a9ab75b489fa
lukaku3 <k4zuhisa@gmail.com>
2021-11-10 22:51:56 +0900

    add ignore, add database setting

--- myiot/settings.py
+++ myiot/settings.py
@@ -78,7 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'admpassword',
         'HOST': 'db',
         'PORT': 5432,
     }
