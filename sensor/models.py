6cf8d09ee3aa18d177d79b5fe6494180eedb09d8
lukaku3 <k4zuhisa@gmail.com>
2021-11-11 16:30:00 +0900

    --

--- sensor/models.py
+++ sensor/models.py
@@ -4,17 +4,20 @@ from django.utils import timezone
 
 # Create your models here.
 class Sensor(models.Model):
+    sensor_type = models.PositiveSmallIntegerField()
     temperature = models.FloatField()
     pressure = models.FloatField()
     humidity = models.FloatField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
-    def __str__(self):
+    def __int__(self):
+        return self.sensor_type
+    def __float__(self):
         return self.temperature
-    def __str__(self):
+    def __float__(self):
         return self.pressure
-    def __str__(self):
+    def __float__(self):
         return self.humidity
     def __str__(self):
         return self.created_at
